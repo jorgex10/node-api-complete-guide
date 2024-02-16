65938384055cfae0c6b1dc10fc448fae03a831d3
Jorge Calvo G. <jorgeecg10@gmail.com>
2024-02-16 13:22:25 -0500

    Adding index for posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,17 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: "First Post", content: "This is the first post!" }],
+    posts: [
+      {
+        _id: 1,
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/post.jpeg",
+        creator: {
+          name: "Jorge",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
