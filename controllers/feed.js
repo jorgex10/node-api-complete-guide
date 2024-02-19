86af2065a7f21a676793daae8cbb79d025a8d982
Jorge Calvo G. <jorgeecg10@gmail.com>
2024-02-19 18:35:21 -0500

    Updating index posts to get them using mongoose

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,20 +3,19 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: 1,
-        title: "First Post",
-        content: "This is the first post!",
-        imageUrl: "images/post.jpeg",
-        creator: {
-          name: "Jorge",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      res
+        .status(200)
+        .json({ message: " Fetched posts syccessfully", posts: posts });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
